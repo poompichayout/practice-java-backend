139c820e4e2eba310f2391a35674620647a8c8fa
poompichayout <poompichayout@gmail.com>
2023-02-16 20:05:22 +0700

    feat: add websocket service

--- src/main/java/com/poompich/training/backend/config/SecurityConfig.java
+++ src/main/java/com/poompich/training/backend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.poompich.training.backend.config;
 
-import com.poompich.training.backend.config.token.TokenFilter;
 import com.poompich.training.backend.config.token.TokenFilterConfigurer;
 import com.poompich.training.backend.service.TokenService;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +10,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     private final TokenService tokenService;
+
+    private final String[] PUBLIC = {
+            "/user/login",
+            "/user/register",
+            "/actuator/**",
+            "/socket/**"
+    };
 
     public SecurityConfig(TokenService tokenService) {
         this.tokenService = tokenService;
@@ -29,10 +39,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.cors().disable().csrf().disable()
+        return http.cors(config -> {
+                    CorsConfiguration cors = new CorsConfiguration();
+                    cors.setAllowCredentials(true);
+                    cors.setAllowedOriginPatterns(Collections.singletonList("http://*"));
+                    cors.addAllowedHeader("*");
+                    cors.addAllowedMethod("GET");
+                    cors.addAllowedMethod("POST");
+                    cors.addAllowedMethod("PUT");
+                    cors.addAllowedMethod("DELETE");
+                    cors.addAllowedMethod("OPTIONS");
+
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", cors);
+
+                    config.configurationSource(source);
+                }).csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeHttpRequests().requestMatchers("/user/login", "/user/register", "/actuator/**").anonymous()
+                .authorizeHttpRequests().requestMatchers(PUBLIC).anonymous()
                 .anyRequest().authenticated()
                 .and()
                 .apply(new TokenFilterConfigurer(tokenService))
---
+++ src/main/java/com/poompich/training/backend/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.poompich.training.backend.model;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ChatMessage {
+
+    private String from;
+
+    private String message;
+
+    private Date created;
+
+    public ChatMessage() {
+        created = new Date();
+    }
+
+}
